fa26b29fd2d0fbb36822826cdc6f92158bcd4f6d
nahikom <94750183+nahikom@users.noreply.github.com>
2023-05-05 19:38:51 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,82 +1,83 @@
 <?php
+// Отправляем браузеру правильную кодировку,
+// файл index.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
-if ($_SERVER['REQUEST_METHOD'] != 'POST'){
-	print_r('Не POST методы не принимаются');
+
+// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
+// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
+  if (!empty($_GET['save'])) {
+    // Если есть параметр save, то выводим сообщение пользователю.
+    print('Спасибо, результаты сохранены.');
+  }
+  // Включаем содержимое файла form.php.
+  include('form.php');
+  // Завершаем работу скрипта.
+  exit();
 }
+// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
+
+// Проверяем ошибки.
 $errors = FALSE;
-if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['year']) || empty($_POST['bio']) || empty($_POST['check1']) || $_POST['check1'] == false || !isset($_POST['super']) ){
-	print_r('Заполните пустые поля!');
-	exit();
+if (empty($_POST['fio'])) {
+  print('Заполните имя.<br/>');
+  $errors = TRUE;
 }
-$name = $_POST['name'];
-$email = $_POST['email'];
-$birth_year = $_POST['year'];
-$pol = $_POST['radio-1'];
-$limbs = intval($_POST['radio-2']);
-$superpowers = array($_POST['super']);
-$bio= $_POST['bio'];
 
-$bioreg = "/^\s*\w+[\w\s\.,-]*$/";
-$reg = "/^\w+[\w\s-]*$/";
-$mailreg = "/^[\w\.-]+@([\w-]+\.)+[\w-]{2,4}$/";
-$list_sup = array('inv','walk','fly');
+if (empty($_POST['year']) || !is_numeric($_POST['year']) || !preg_match('/^\d+$/', $_POST['year'])) {
+  print('Заполните год.<br/>');
+  $errors = TRUE;
+}
 
-if(!preg_match($reg,$name)){
-	print_r('Неверный формат имени');
-	exit();
+if (empty($_POST['email']) || !preg_match('/^((([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1})|([0-9А-Яа-я]{1}[-0-9А-я\.]{1,}[0-9А-Яа-я]{1}))@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/u',$_POST['email'])) {
+  print('Зфполните email.<br/>');
+  $errors = TRUE;
 }
-if($limbs == 0){
-	print_r('Нет конечностей');
-	exit();
+
+if (empty($_POST['gender']) || ($_POST['gender']!='m' && $_POST['gender']!='w')) {
+  print('Заполните пол.<br/>');
+  $errors = TRUE;
 }
-if(!preg_match($bioreg,$bio)){
-	print_r('Неверный формат биографии');
-	exit();
+if (empty($_POST['limbs']) || ($_POST['limbs']!='1' && $_POST['limbs']!='2' && $_POST['limbs']!='3' && $_POST['limbs']!='4')) {
+  print('Заполните количество конечностей.<br/>');
+  $errors = TRUE;
 }
-if(!preg_match($mailreg,$email)){
-	print_r('Неверный формат email');
-	exit();
+
+if (empty($_POST['biography']) || !preg_match('/^([0-9a-zA-Zа-яА-Я\,\.\s]{1,})$/', $_POST['biography']) ){
+  print('Заполните биографию.<br/>');
+  $errors = TRUE;
 }
-if($pol !== 'male' && $pol !== 'female'){
-	print_r('Неверный формат пола');
-	exit();
+if (empty($_POST['ability'])) {
+  print('Заполните сферхспособности.<br/>');
+  $errors = TRUE;
 }
-foreach($superpowers as $checking){
-	if(array_search($checking,$list_sup)=== false){
-			print_r('Неверный формат суперсил');
-			exit();
-	}
+
+
+
+// *************
+// Тут необходимо проверить правильность заполнения всех остальных полей.
+// *************
+
+if ($errors) {
+  // При наличии ошибок завершаем работу скрипта.
+  exit();
 }
+
+// Сохранение в базу данных.
 
 $user = 'u52996';
 $pass = '6060818';
-$db = new PDO('mysql:host=localhost;dbname=u52996', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
-try {
-  $stmt = $db->prepare("INSERT INTO form SET name=:name, email=:email, year=:byear, pol=:pol, limbs=:limbs, bio=:bio");
-  $stmt->bindParam(':name', $name);
-  $stmt->bindParam(':email', $email);
-  $stmt->bindParam(':byear', $birth_year);
-  $stmt->bindParam(':pol', $pol);
-  $stmt->bindParam(':limbs', $limbs);
-  $stmt->bindParam(':bio', $bio);
+$db = new PDO('mysql:host=localhost;dbname=u52996', $user, $pass, [PDO::ATTR_PERSISTENT => true]);
 
-  if($stmt->execute()==false){
-  print_r($stmt->errorCode());
-  print_r($stmt->errorInfo());
-  exit();
-  }
-  
-  
-  $id = $db->lastInsertId();
-  $sppe= $db->prepare("INSERT INTO super SET name=:name, per_id=:person");
-  $sppe->bindParam(':person', $id);
-  foreach($superpowers as $inserting){
-	$sppe->bindParam(':name', $inserting);
-	if($sppe->execute()==false){
-	  print_r($sppe->errorCode());
-	  print_r($sppe->errorInfo());
-	  exit();
-	}
+// Подготовленный запрос. Не именованные метки.
+try {
+  $stmt = $db->prepare("INSERT INTO application SET name = ?, email=?, year=?, gender=?, biography=?, limbs=?");
+  $stmt -> execute([$_POST['fio'], $_POST['email'],$_POST['year'],$_POST['gender'], $_POST['biography'],$_POST['limbs'] ]);
+  $app_id = $db->lastInsertId();
+  $stmt = $db->prepare("INSERT INTO ability_application SET ability_id= ?, application_id=?");
+  foreach ($_POST['ability'] as $ability) {
+    $stmt->execute([$ability, $app_id]);
   }
 }
 catch(PDOException $e){
@@ -84,5 +85,24 @@ catch(PDOException $e){
   exit();
 }
 
-print_r("Данные отправлены в бд");
-?>
+//  stmt - это "дескриптор состояния".
+ 
+//  Именованные метки.
+//$stmt = $db->prepare("INSERT INTO test (label,color) VALUES (:label,:color)");
+//$stmt -> execute(['label'=>'perfect', 'color'=>'green']);
+ 
+//Еще вариант
+/*$stmt = $db->prepare("INSERT INTO users (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
+$stmt->bindParam(':firstname', $firstname);
+$stmt->bindParam(':lastname', $lastname);
+$stmt->bindParam(':email', $email);
+$firstname = "John";
+$lastname = "Smith";
+$email = "john@test.com";
+$stmt->execute();
+*/
+
+// Делаем перенаправление.
+// Если запись не сохраняется, но ошибок не видно, то можно закомментировать эту строку чтобы увидеть ошибку.
+// Если ошибок при этом не видно, то необходимо настроить параметр display_errors для PHP.
+header('Location: ?save=1');
